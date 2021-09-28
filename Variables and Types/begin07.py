866726d4e54eb9ebe97478d730af83cea2dcdda7
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-09-27 23:12:14 -0700

    begin tasks answer

---
+++ Variables and Types/begin07.py
@@ -0,0 +1,10 @@
+# a o'zgaruvchiga 5 ni taminlang;
+# b o'zgaruvchiga 4 ni taminlang;
+# c o'zgaruvchiga 8 ni taminlang;
+# Quyidagi ifodani natijaga chop eting;
+# ifoda: (2*a/b)*c
+
+a = 5
+b = 4
+c = 8
+print((2*a/b)*c)
