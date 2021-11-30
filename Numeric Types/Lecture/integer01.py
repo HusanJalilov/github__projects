c4d7dd114784c599a7eb3c415885974c279c174d
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-11-30 16:23:13 +0500

    Update integer01.py
    yangi

--- Numeric Types/Lecture/integer01.py
+++ Numeric Types/Lecture/integer01.py
@@ -1,5 +1,5 @@
 import math
-a=int(input("a ning qiymatini kiriting:  "))
-b=int(input("b ning qiymatini kiriting:  "))
-s=math.sqrt(math.sqrt(pow(a,3)-pow(b,3)+pow(a,3)+pow(b,3)))
+a=int(input("a ning qiymatini kiriting:  "))  # a ning qiymatini kiriting
+b=int(input("b ning qiymatini kiriting:  "))  # b ni qiymatini kiriting
+s=math.sqrt(math.sqrt(pow(a,3)-pow(b,3)+pow(a,3)+pow(b,3)))  # amallar qatori
 print(s)
