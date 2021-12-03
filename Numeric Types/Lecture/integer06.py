6b3e30a1bdabde6bb8e6547a6efe96acf8ac9f8f
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-03 23:01:14 +0500

    Update integer06.py
    submit

--- Numeric Types/Lecture/integer06.py
+++ Numeric Types/Lecture/integer06.py
@@ -3,5 +3,5 @@ r=int(input("r ning qiymatini kiriting:  "))
 n=int(input("n ning qiymatini kiriting:  "))
 a=int(input("a ning qiymatini kiriting:  "))
 b=int(input("b ning qiymatini kiriting:  "))
-c=(pow((1+r/100),n))/(math.sqrt(a**2+b**2))
+c=(pow((1+r/100),n))/(math.sqrt(a**2+b**2))  #amallar to'plami
 print(c)
